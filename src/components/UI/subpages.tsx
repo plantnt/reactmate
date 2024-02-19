81fde5141c819eeee894dff8dcd062d566d8ede4
plantnt <godzillagmk2204@gmail.com>
2024-02-19 18:20:05 -0500

    modified:   src/components/UI/navbar.tsx 	modified:   src/components/UI/subpages.tsx 	new file:   src/components/UI/ui.css

--- src/components/UI/subpages.tsx
+++ src/components/UI/subpages.tsx
@@ -1,19 +1,18 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
 import { Layers, MessagesSquare } from "lucide-react"
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function SubpageSelect(){  
     return(
         <>
-            <DropdownButton className="max-w-[120px] 
-                            bg-furniorange p-2 pl-2 
-                            text-black font-bold rounded-lg" title="Catalogo">
-                <Dropdown.Item>Action</Dropdown.Item>
-                <Dropdown.Item>Another action</Dropdown.Item>
-                <Dropdown.Item>Something else</Dropdown.Item>
-            </DropdownButton>
+            <div className="dropdown">
+                <button className="flex">
+                    <Layers size={20} className="mr-2"/> Catalogo
+                </button>
+                <div className="dropdown-menu">
+
+                </div>
+            </div>
         </>
     )
     
