f7f6e9c2bf33f261629cb2bf3871fef11eded055
alejiaet <alejiaet@gmail.com>
2024-02-21 14:31:08 -0500

    creacion subir producto 	new file:   src/components/addProduct.tsx 	modified:   src/pages/Home.tsx

---
+++ src/components/addProduct.tsx
@@ -0,0 +1,11 @@
+export default function AddProduct(){
+  return(
+
+<select className="bg-furniorange absolute rounded-full hover:bg-orange-500">
+  <option value="someOption">+</option>
+  <option value="otherOption">Subir un producto</option>
+  <option value="otherOption">Ver mis productos</option>
+</select>
+
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Filters from "../components/filters"
 import Footer from "../components/UI/footer"
 import Rating from "../components/rating"
 import FeaturedProducts from "./Featured"
+import AddProduct from "../components/addProduct"
 
 import './styles/home.css'
 
@@ -36,6 +37,7 @@ const Home = () => {
                             <ProductCard />
                             <ProductCard />
                         </div>
+                        <AddProduct/>
                     </div>
                 </div>
                 <Pagination className="self-center mt-6" defaultCurrent={1} total={50}></Pagination>
