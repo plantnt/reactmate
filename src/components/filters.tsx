b37b943f907cd95dac062e5d2e417493bffef079
mixsw <143354988+r41nbows@users.noreply.github.com>
2024-02-21 20:59:26 -0500

    crazy? i was crazy once
    i was locked on a room

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,24 +1,25 @@
-import { PlusCircle } from "lucide-react"
-export default function Filters(){
-    return(
+import { PlusCircle } from "lucide-react";
+
+export default function Filters() {
+    return (
         <div>
-            <div id="filters" className="w-[17rem] py-5 pl-5 pr-10 border-r-[1px] border-b-[1px] border-slate-300 bg-white ">
+            <div id="filters" className="w-[17rem] py-5 pl-5 pr-10 border-r-[1px] border-b-[1px] border-t-[1px] border-slate-300 bg-white rounded-lg">
                 <h2 className="font-bold mb-3">Categorías</h2>
                 <ul className="flex flex-col space-y-2">
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Muebles de hogar
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Muebles de exteriores
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Muebles de madera
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Muebles de plástico
                     </li>
                     <li>
@@ -31,19 +32,19 @@ export default function Filters(){
                 <h2 className="font-bold mt-10 mb-3">Color</h2>
                 <ul className="flex flex-col space-y-2">
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Negro
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Gris
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Café
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Verde
                     </li>
                     <li>
@@ -56,19 +57,19 @@ export default function Filters(){
                 <h2 className="font-bold mt-10 mb-3">Material</h2>
                 <ul className="flex flex-col space-y-2">
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Madera
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Madera reciclada
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Plástico reciclado
                     </li>
                     <li>
-                        <input type="checkbox" className="accent-violet-500 w-5 mr-2"/>
+                        <input type="checkbox" className="accent-violet-500 w-5 mr-2 transition-all duration-300"/>
                         Cuero
                     </li>
                     <li>
@@ -78,9 +79,7 @@ export default function Filters(){
                         </label>
                     </li>
                 </ul>
-                
             </div>
-                
         </div>
-    )
+    );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,38 +1,143 @@
+import { useState, useRef, useEffect } from 'react';
+import { FaRegCommentDots, FaRegListAlt, FaEllipsisH } from 'react-icons/fa';
+import ProductCard from "../components/productCard";
+import Filters from "../components/filters";
+import Footer from "../components/UI/footer";
+import Rating from "../components/rating";
+import { Pagination } from 'antd';
+
 export default function Profile() {
-    return (
-      <div className="bg-gray-100 min-h-screen py-8">
-        <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClose = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClose);
+    return () => {
+      document.removeEventListener('mousedown', handleClose);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="bg-gray-50 min-h-screen py-8"> {/* Cambio del color de fondo a bg-gray-50 */}
+        <div className="max-w-4xl mx-auto bg-white rounded-lg border-2 border-gray-200 overflow-hidden">
+        <br></br>
           <div className="md:flex">
-            <br></br><br></br>
             <div className="md:flex-shrink-0">
               <img
                 className="h-48 w-48 object-cover rounded-lg mx-auto"
-                src="https://via.placeholder.com/150"
+                src="src/assets/profileIcon.png"
                 alt="Profile"
               />
             </div>
-            <div className="p-8">
-              <div className="uppercase tracking-wide text-sm text-indigo-500 font-bold">
-                donjuan700
+            <div className="p-8 text-center">
+              <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+                <h1 className="text-2xl">donjuan700</h1>
               </div>
-              <p className="text-indigo-700 flex">VENDEDOR<img width="15px" src="src/assets/cartIcon.png"></img></p>
-              <p className="mt-2 text-gray-600">
+              <p className="text-indigo-700 font-semibold">VENDEDOR</p>
+              <br />
+              <p className="mt-2 text-gray-500 font-semibold">
                 Esta es la página destinada para mostrar los datos introducidos en el registro
                 como un usuario completo, desde la vista del usuario primario en su propia página
+                OJO Q EL FORO EXTERNO ES SOLAMENTE TEMPORAL
               </p>
+              <br />
+
+              <div className="mt-4 text-center">
+                <p className="text-gray-700">
+                  <span className="font-semibold">Correo electrónico:</span> donjuan@example.com
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Teléfono:</span> +57 345 6789034
+                </p>
+                <p className="text-gray-600">
+                  <span className="font-semibold">Fecha de Registro:</span> 20 de febrero de 2024
+                </p>
+              </div>
               <br></br>
+
               <div className="mt-4 flex justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Abrir Foro
-                </button>       
-                  <a href="http://localhost:3000/"><button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Ver Productos</button></a>
-                <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                  Opciones... desplegable</button>
+                <a href="https://furnimate.foroactivo.com/">
+                  <button className="bg-blue-500 hover:bg-white hover:text-blue-500 hover:border-blue-500 transition duration-300 border-2 border-blue-500 text-white font-bold py-2 px-4 rounded flex items-center">
+                    <FaRegCommentDots className="inline-block mr-2 text-xl" />
+                    <span className="text-base">Abrir Foro</span>
+                  </button>
+                </a>
+                <a href="http://localhost:3000/">
+                  <button className="bg-green-500 hover:bg-white hover:text-green-500 hover:border-green-500 transition duration-300 border-2 border-green-500 text-white font-bold py-2 px-4 rounded flex items-center justify-center">
+                    <FaRegListAlt className="inline-block mr-2 text-xl" />
+                    <span className="text-base">Ver en Catálogo</span>
+                  </button>
+                </a>
+
+                <div ref={dropdownRef} className="">
+                  <button
+                    onClick={handleClick}
+                    className="bg-gray-500 hover:bg-white hover:text-gray-500 hover:border-gray-500 transition duration-300 border-2 border-gray-500 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
+                  >
+                    <span className="text-base">Opciones</span>
+                    <FaEllipsisH className="inline-block ml-2 text-xl" />
+                  </button>
+                  {isOpen && (
+                    <div
+                      className="absolute left-500 bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg z-10"
+                    >
+                      <div className="py-1">
+                        <p className="text-sm px-4 py-2 hover:bg-gray-100 cursor-pointer">Modificar perfil</p>
+                        <p className="text-sm px-4 py-2 hover:bg-gray-100 cursor-pointer">Modificar contraseña</p>
+                        <p className="text-sm px-4 py-2 hover:bg-gray-100 cursor-pointer">Eliminar cuenta</p>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-            <div className="p-4 text-gray-600">Fecha de registro: 20 Febrero 2024</div>
+          </div>
+        </div>
+
+        {/* Esta es la linea separadora q separa jaajaj */}
+        <hr className="my-8 border-b border-gray-200 mx-4" />
+
+        <h1 className="text-center text-gray-400">*Se filtrarán solamente los productos de este usuario*</h1>
+        <br></br>
+
+        <div className="h-full w-full flex flex-col">
+          <div className="grid grid-flow-col">
+            <Filters />
+            <div className="max-w-[60rem] justify-self-start">
+              <div className="mt-3">
+                <Rating />
+              </div>
+              <div className="max-w-[90%] inline-flex flex-wrap gap-3 ml-3 pt-3">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+              </div>
+            </div>
+          </div>
+          <Pagination className="self-center mt-6" defaultCurrent={1} total={50}></Pagination>
+          <div className="grid w-full justify-self-end">
+            <Footer />
           </div>
         </div>
       </div>
-    );
-  }
+    </>
+  );
+}
