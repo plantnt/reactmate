bd933d1ae2fbc948700abab8672662e1a13a3d6f
alejiaet <alejiaet@gmail.com>
2024-02-29 08:12:15 -0500

    featured terminado, logo pagina sin añadir modified:   index.html new file:   src/assets/favicon.ico new file:   src/assets/furniLogo.png modified:   src/pages/Featured.tsx

--- src/pages/Featured.tsx
+++ src/pages/Featured.tsx
@@ -7,12 +7,12 @@ export default function FeaturedProducts(){
  return(
     <>
 <div className="w-full bg-gradient-to-r from-furnipurple to-furniorange rounded min-h-[250px] my-4 p-8">
-    <h1 className="text-white text-2xl pb-5 font-bold antialiased">Productos Destacados</h1>
+    <h1 className="text-white text-2xl pb-5 font-bold antialiased">PRODUCTOS DESTACADOS</h1>
 
-    <div className="flex">
+    <div className="flex space-x-10 justify-center">
 
-       <div className="border-4 border-yellow-500">
-         <button className="bg-white rounded-full h-10 w-10">
+       <div className="pr-8 flex justify-center items-center">
+         <button className="bg-gray-700 bg-opacity-15 rounded-full h-10 w-10 p-2 flex justify-center items-center text-white hover:bg-opacity-40 ease-in-out duration-1000">
          <SlArrowLeft />
          </button>
        </div>
@@ -23,11 +23,13 @@ export default function FeaturedProducts(){
         <ProductCard />
         <ProductCard />
         <ProductCard />
+      
+       
         
             
 
-       <div className="border-4 border-yellow-500 center align-center">
-         <button className="bg-white rounded-full h-10 w-10 justify-self-center">
+       <div className="pl-8 flex justify-center items-center">
+         <button className="bg-gray-700 bg-opacity-15 rounded-full h-10 w-10 p-2 flex justify-center items-center text-white hover:bg-opacity-40 ease-in-out duration-1000">
          <SlArrowRight/>
          </button>
        </div>
