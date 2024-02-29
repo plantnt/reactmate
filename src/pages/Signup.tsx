9b16e2ef5e5b75278f7420ef45f1ffff0f29d18e
tgodzila22 <143355009+tgodzila22@users.noreply.github.com>
2024-02-29 07:40:17 -0500

    me esta quedando del culo estp

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
 import { message } from "antd";
+import React from "react";
 
 const supabase = createClient("https://bvbyifqplonojbgaeafg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2YnlpZnFwbG9ub2piZ2FlYWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MzA2ODAsImV4cCI6MjAyNDAwNjY4MH0.16txlFiUZ7kfJuCDe8G5aWu2gUg_455R6dmp5f5nFHY");
 
