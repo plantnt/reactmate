ea8264ae9f0ecc76be911a08f586382d00fb6914
alejiaet <alejiaet@gmail.com>
2024-03-01 13:27:32 -0500

    new file:   src/pages/ProductUpload.tsx

---
+++ src/pages/ProductUpload.tsx
@@ -0,0 +1,11 @@
+export default function Upload(){
+
+  return(
+      <> 
+      <div className="flex border-4 border-gray-800 w-[100px] h-[200px]">
+        <h1>AAAAA</h1>
+      </div>
+      </>
+ )
+}
+ b
