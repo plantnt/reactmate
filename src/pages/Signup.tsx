5bd7ba33b3d1371cbef8e55fcd364190311f95a2
plantnt <godzillagmk2204@gmail.com>
2024-03-08 14:31:26 -0500

    deleted:    src/assets/SignUpBackground.jpg 	modified:   src/pages/Signup.tsx 	src/assets/SignUpBackground.png

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import { IoMdImage } from "react-icons/io";
+import { IoMdImage, IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import { FaCircleCheck } from "react-icons/fa6";
 import { supabase } from "../utils/Utils";
@@ -16,7 +16,7 @@ const initialState = {
 
 export default function SignUp(){
     const [formData, setFormData] = useState(initialState)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const [status, setStatus] = useState(false)
 
     const handleChange = async (e:any) => {
@@ -68,10 +68,16 @@ export default function SignUp(){
 }
 console.log(loading)
 
+const [visible, setVisible] = useState(true)
+const handleClick = () => {
+    setVisible((prevVisible) => !prevVisible)
+}
+
+
 return(
-        <div className="flex items-center justify-center w-full h-[100vh] bg-cover bg-center bg-[url('/src/assets/SignUpBackground.jpg')]">
-            <div className="flex flex-col items-start relative w-[78%] rounded-2xl p-9 text-md bg-white shadow-md">
-                <img src="src/assets/textLogo.png" className="md:h-[40px] md:w-[200px] mb-2 sm:justify-self-center" />
+        <div className="flex items-center justify-center w-full h-[100vh] lg:bg-cover sm:bg-left  bg-[url('/src/assets/SignUpBackground.png')]">
+            <div className="flex flex-col items-start relative rounded-2xl p-9 text-md bg-white w-[90%] max-w-[900px] bg-opacity-80 backdrop-blur-sm shadow-md">
+                <img src="src/assets/textLogo.png" className="md:h-[40px] md:w-[220px] mb-2 sm:justify-self-center" />
                 <NavLink to="/">
                     <IoClose size={25} className="text-slate-600 absolute top-2 right-2 hover:scale-105"/>
                 </NavLink>
@@ -81,48 +87,56 @@ return(
                             createUser()
                         }}>
                     <div className="flex flex-col items-center text-sm space-y-2">
-                        <div className="group flex flex-col items-center justify-center self-center h-[100px] w-[100px] rounded-full border hover:bg-slate-100 transition-colors cursor-pointer"
+                        <div className="group flex flex-col items-center justify-center self-center h-[100px] w-[100px] rounded-full border border-slate-300 hover:bg-opacity-60 transition-colors cursor-pointer"
                             onClick={(e) => handleImage(e)}>
                             <IoMdImage  size={37} className="text-slate-600 group-hover:scale-105 group-hover:text-violet-600 transition-all"/>
                             <p className="mt-2 text-xs text-slate-600 text-wrap group-hover:scale-105 group-hover:text-violet-600 transition-all">Añadir foto</p>
                         </div>  
                         <p className="select-none">Foto de perfil</p>
-                    <span className="w-[90%] max-w-[700px] bg-white border-t border-t-gray-300 rounded-lg"></span>
+                    <span className="w-[90%] max-w-[700px] bg-white border-t border-t-slate-300 rounded-lg"></span>
                     </div>
                     <div className="w-full grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-6">
                         <label className="flex flex-col">
                             <p className="select-none">Nombre</p> 
-                            <input type="text" className="w-[400px] outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}
-                                onChange={handleChange} value={formData.firstName ?? ""}/>
+                            <input type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent"/>
                         </label>
                         <label className="flex flex-col">
                             <p className="select-none">Dirección de correo</p> 
-                            <input type="text" className="w-[400px] outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}
-                                onChange={handleChange} value={formData.email ?? ""}/>
+                            <input type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange}/>
                         </label>
                         <label className="flex flex-col">
                             <p className="select-none">Apellido</p> 
-                            <input type="text" className="w-[400px] outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}
-                                onChange={handleChange} value={formData.lastName ?? ""} required/>
+                            <input type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange} required/>
                         </label>
-                        <label className="flex flex-col">
-                            <p className="select-none">Contraseña</p> 
-                            <input type="password" className="w-[400px] outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}
-                                onChange={handleChange} value={formData.password ?? ""}/>
+                        <label className="relative flex flex-col">
+                            <p className="select-none">Contraseña</p>
+                            <input type={!visible ? "text" : "password"} className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange}/>
+                            {visible === true ? (
+                                <IoMdEye size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            ) : (
+                                <IoMdEyeOff size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            )}
                         </label>
                     </div>
-                    <button className="flex justify-self-center justify-center w-[40%] min-w-[100px] mt-[20px] bg-violet-400 px-4 py-2 rounded-md text-white font-semibold" type="submit" disabled={loading}>
-                        registrarse
+                    <button className="flex justify-self-center justify-center w-[40%] min-w-[100px] mt-[20px] bg-violet-400 px-4 py-2 rounded-md text-white font-semibold hover:bg-violet-500 transition-colors" 
+                            type="submit" 
+                            >
+                        Registrarse
                     </button>
                     <p className="justify-self-center w-[200px] mt-8 text-sm text-wrap text-center">¿Ya tienes una cuenta? prueba con <NavLink to="/logIn"><span className="text-violet-600">iniciar sesión</span></NavLink></p>
                 </form>
             </div>
-            {status && (
-                <div className="absolute bottom-1 right-1 flex items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-20 translate-y-20">
+            {status === false? (
+                <div className="hidden absolute bottom-2 right-2 items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-20 translate-y-20 transition-transform"></div>
+                ) : (
+                <div className="fixed bottom-2 right-2 flex items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-0 translate-y-0 transition-transform">
                     <FaCircleCheck size={20} className="text-green-400" />
                     Se ha registrado correctamente
                 </div>
-            )}
+                )}
         </div>
     )
 }
