e31eb55ccb61e0088faee9b79ba002f51b0199cb
ra1nbows <143354988+r41nbows@users.noreply.github.com>
2024-03-12 07:37:25 -0500

    glow up

--- src/components/cardcart.tsx
+++ src/components/cardcart.tsx
@@ -1,17 +1,22 @@
+import { FaEllipsisVertical } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 // Componente de Carrito
 export default function CardCart() {
-    const title = "Ejemplo de Producto";
-    const price = 9999;
+    const title = "Un gran Sofá gris";
+    const price = "10300";
 
     const handleDelete = () => {
         // Lógica para eliminar el producto del carrito
     };
 
     return (
-        <div className="card border-4 rounded-r-3xl rounded-l-3xl w-7/12 p-4 m-2 flex items-center">
+        <div className="border-2 bg-gray-50 rounded-r-3xl rounded-l-3xl w-7/12 p-4 m-4 flex items-center justify-between">
             {/* Componente de Producto en el Carrito */}
+
+            <FaEllipsisVertical className="-mr-10 text-gray-400"/>
+            <FaEllipsisVertical className="text-gray-400"/>
+
             <div className="max-w-[200px] rounded-xl border border-slate-200 cursor-pointer hover:transform">
                 <NavLink to="/productPage">
                     <img src="src/assets/products/2.jpg" className="w-full rounded-t-xl" alt="Product Image" />
@@ -25,12 +30,14 @@ export default function CardCart() {
             </div>
 
             {/* Botón para Ver Producto */}
-            <NavLink to="/productPage" className="ml-auto mr-4 font-bold bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                Ver Producto
+            <NavLink to="/productPage">
+                <button className="bg-orange-400 hover:bg-white hover:text-orange-400 hover:border-orange-400 transition duration-300 border-2 border-orange-400 text-white font-bold py-2 pl-4 px-4 rounded items-center">
+                  <span className="text-base font-rounded">Ver producto</span>
+                </button>
             </NavLink>
 
             {/* Botón para Eliminar Producto */}
-            <button onClick={handleDelete} className="bg-red-500 font-bold text-white py-2 px-4 rounded hover:bg-red-600">
+            <button onClick={handleDelete} className="border-red-500 bg-red-500 hover:bg-white hover:text-red-500 hover:border-red-500 transition duration-400 border-2 font-bold text-white py-2 px-4 rounded hover:bg-red-600">
                 Eliminar
             </button>
         </div>
