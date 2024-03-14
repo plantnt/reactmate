66bad7aa486afbf9cad41f73ddf8f329ecf2f7d8
tgdzila22 <tgodzila22@.com>
2024-03-14 18:55:36 -0500

    modified:   src/pages/Login.tsx 	modified:   src/pages/ProductUpload.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,42 +1,118 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { supabase } from "../utils/Utils";
+
+import { IoMdEye, IoMdEyeOff, IoIosClose } from "react-icons/io";
+
+import googleIcon from '../assets/icons/google.svg'
+import Separator from "../components/UI/separator";
 
 export default function LogIn(){
-    return(
-        <>  
+    
+    const [visible, setVisible] = useState(true)
+    const handleClick = () => {
+        setVisible((prevVisible) => !prevVisible)
+    }
+    
+    const handleLogin = () =>{
+        supabase.auth.signInWithOAuth({
+            provider: 'google',
+          })
+          
+    }
 
-<div className="w-full h-full bg-[url('/src\assets\LoginBackground.jpg')] bg-cover bg-[50%] absolute">
-    <div className="flex flex-col items-center  mt-10">
+let navigate = useNavigate()
 
-    <NavLink to="/" className="max-w-96 pb-10">
-        <img src="./src\assets\textLogo.png" className="p-4"></img>
-    </NavLink>
+const [formData,setFormData] = useState({
+    email:'',password:''
+})
 
-    <div className="flex flex-col bg-gray-200 p-4 rounded-2xl m-0-auto w-[30%] items-center text-center m-5 h-full">
-        <label className="text-2xl font-bold pb-4 text-violet-700">
-            Inicio de sesión
-        </label>
-        <ul className="w-full">
-            
-            <li className="p-2">
-                <input type="text" className="border-2 border-gray-400 rounded p-1 w-full" placeholder="Nombre de Usuario/Correo Electronico"/>
-            </li> 
-            <li className="p-2">
-                <input type="password" className="border-2 border-gray-400 rounded p-1 w-full" placeholder="Contraseña"/>
-            </li> 
+console.log(formData)
 
-        </ul>
-        <button className="bg-violet-600 hover:bg-violet-800 border-2 border-violet-900 rounded p-1 text-white w-full my-2 transition-all"> Iniciar Sesión </button>
-            <div className="flex">
-                <label>¿No tienes una cuenta?</label>
-                <NavLink to="/signUp" className="text-blue-400 pl-2">Registrarse</NavLink>
+function handleChange(event: { target: { name: any; value: any; }; }){
+setFormData((prevFormData)=>{
+  return{
+    ...prevFormData,
+    [event.target.name]:event.target.value
+  }
+
+})
+
+}
+
+async function handleSubmit(e:any){
+    e.preventDefault()
+
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: formData.email,
+            password: formData.password,
+          })
+
+      if (error) throw error
+      console.log(data)
+      setToken(data)
+      navigate('/homepage')
+
+
+    //   alert('Check your email for verification link')
+
+      
+    } catch (error) {
+      alert(error)
+    }
+  }
+  
+  function setToken(_data: { user: import("@supabase/gotrue-js").User; session: import("@supabase/gotrue-js").Session; weakPassword?: import("@supabase/gotrue-js").WeakPassword | undefined; }) {
+    throw new Error("Function not implemented.");
+}
+
+return(
+        <div className="flex items-center justify-center w-full h-[100vh] lg:bg-cover sm:bg-left  bg-[url('/src/assets/SignUpBackground.png')]">
+            <div className="flex flex-col items-start relative rounded-2xl p-9 text-md bg-white w-[90%] max-w-[900px] bg-opacity-80 backdrop-blur-sm shadow-md">
+                <img src="src/assets/textLogo.png" className="md:h-[40px] md:w-[220px] mb-2 sm:justify-self-center" />
+                <NavLink to="/">
+                    <IoIosClose  size={28} className="text-slate-400 absolute top-3 right-3 hover:scale-105 hover:text-red-600 transition-all"/>
+                </NavLink>
+                <div className="flex flex-col items-center w-full">
+                    <button className="flex items-center justify-center text-lg bg-white py-2 w-[260px] mb-[40px] rounded-full hover:bg-opacity-70 hover:border-4 hover:border-violet-400 transition-all" onClick={() => handleLogin()}>
+                        <img src={googleIcon} className="h-[30px] mr-4"/>
+                        Google
+                    </button>
+                    <Separator />
+                </div>
+                <form className="grid w-full space-y-3 mt-[20px]" onSubmit={handleSubmit}>
+                    <div className="w-full grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-6">
+                        <label className="flex flex-col">
+                            <p className="select-none">Email</p> 
+                            <input name="email" type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent"
+                            onChange={handleChange}/>
+                        </label>
+                        <label className="relative flex flex-col">
+                            <p className="select-none">Contraseña</p>
+                            <input name="password" type={!visible ? "text" : "password"} className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange}/>
+                            {visible === true ? (
+                                <IoMdEye size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            ) : (
+                                <IoMdEyeOff size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            )}
+                        </label>
+                    </div>
+                    <button className="flex justify-self-center justify-center w-[40%] min-w-[100px] bg-violet-400 px-4 py-2 rounded-md text-white font-semibold hover:bg-violet-500 transition-colors" 
+                            type="submit" 
+                            >
+                        Iniciar sesión
+                    </button>
+                    <p className="justify-self-center w-[200px] mt-8 text-sm text-wrap text-center">Aún no tienes cuneta? <br/>Creala <NavLink to="/signUp"><span className="text-violet-600">aquí</span></NavLink></p>
+                </form>
             </div>
             <div>
                 <NavLink to="#" className="text-blue-400 pl-2">¿Olvido su Contraseña?</NavLink>
             </div>
     </div>
-</div>
-    </div>
-            
-        </>
+
     )
 }
+
+
--- src/pages/ProductUpload.tsx
+++ src/pages/ProductUpload.tsx
@@ -9,6 +9,7 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import { GiCancel } from "react-icons/gi";
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import { supabase } from '../utils/Utils';
 
 export default function Upload(){
 
@@ -36,29 +37,93 @@ export default function Upload(){
     { value: '4', label: 'Plastico Reciclado' }
   ]
 
-  const [value, setValue] = useState('')
-  const handleChange = (event: { target: { value: string } }) => {
-      const result = event.target.value.replace(/\D/g, '')
-      setValue(result)
-  }
+  // const [value, setValue] = useState('')
+  // const handleChangeP = (event: { target: { value: string } }) => {
+  //     const result = event.target.value.replace(/\D/g, '')
+  //     setValue(result)
+  // }
+
+  const initialState = {
+    title: "",
+    description: "",
+    price: "",
+    image: null,
+}
+
+
+    const [formData, setFormData] = useState(initialState)
+    const [loading, setLoading] = useState(true)
+    const [status, setStatus] = useState(false)
+
+    const handleChange = async (e:any) => {
+        e.preventDefault()
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+    
+    const handleImage = async (e:any) => {
+        const reader = new FileReader()
+        reader.onloadend = async () => {
+            setLoading(true)
+        }
+        
+        const files = e.target.files[0]
+        if(!files) return
+        const data = new FormData()
+        data.append('file', files) 
+        data.append('upload_preset', 'c_tags')
+        const res = await fetch(
+            "https://api.cloudinary.com/v1_1/dotrqr59h/upload",
+            {
+                method: "POST",
+                body: data
+            }
+        ) 
+        
+        const file = await res.json()
+        setFormData({...formData, image: file.secure_url})
+        setLoading(false)
+    }
+        
+    const createProduct = async () => {
+        const res = await supabase.from("users").insert([
+            {
+                image: `${formData.image}`,
+                title: `${formData.title}`,
+                description: `${formData.description}`,
+                price: `${formData.price}`,
+            }
+        ])
+        if (res.error === null && res.status === 201) {
+            setStatus(true);
+            setFormData(initialState);
+            setTimeout(() => {
+              setStatus(false);
+            }, 5000);
+    }
+}
+console.log(loading)
 
   return(
     <> 
       <div className="flex space-x-9 p-8">
-          <form className='grid grid-cols-3'>
+          <form className='grid grid-cols-3' onSubmit={(e:any) => {
+                            e.preventDefault()
+                            createProduct()
+                        }}>
           <UploadWidget />
           <div className='ml-4'>
             <label className='grid'>
               Título del producto
-              <input type="text" id='title' className='border border-slate-300 h-[2em] outline-none p-1 rounded-md' required/>
+              <input type="text" id='title' name='title' className='border border-slate-300 h-[2em] outline-none p-1 rounded-md' required 
+              onChange={handleChange}/>
             </label>
             <label className='relative grid'>
               Precio
-              <input type="text" value={value} onChange={handleChange} className="border border-slate-300 h-[2em] outline-none p-1 rounded-md" maxLength={10} placeholder='$'/>
+              <input type="text" name='price' onChange={handleChange} className="border border-slate-300 h-[2em] outline-none p-1 rounded-md" maxLength={10} placeholder='$'/>
             </label>
             <label className='grid'>
-              Descrición
-              <textarea id='description' className='border border-slate-300 resize-none outline-none p-1 rounded-lg' cols={30} rows={10} maxLength={500}></textarea>
+              Descripción
+              <textarea id='description' name='description' className='border border-slate-300 resize-none outline-none p-1 rounded-lg' cols={30} rows={10} maxLength={500} onChange={handleChange}></textarea>
             </label>
           </div>
           <div className='flex flex-col space-y-4 ml-4'>
