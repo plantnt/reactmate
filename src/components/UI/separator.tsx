1bae7aa3b4560b82cd5552c2915ab587c5388c93
tgodzila22 <143355009+tgodzila22@users.noreply.github.com>
2024-03-15 08:36:54 -0500

    new file:   src/assets/icons/google.svg 	new file:   src/components/UI/separator.tsx 	modified:   src/pages/Signup.tsx

---
+++ src/components/UI/separator.tsx
@@ -0,0 +1,7 @@
+const Separator = () => {
+    return (
+        <span className="w-[90%] max-w-[700px] bg-white border-t border-t-slate-300 rounded-lg"></span>
+    )
+}
+
+export default Separator
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import { IoMdImage } from "react-icons/io";
-import { IoClose } from "react-icons/io5";
+import { IoMdImage, IoMdEye, IoMdEyeOff, IoIosClose } from "react-icons/io";
 import { FaCircleCheck } from "react-icons/fa6";
 import { supabase } from "../utils/Utils";
+import Separator from "../components/UI/separator";
+
 
 const initialState = {
     firstName: "",
@@ -36,7 +37,7 @@ export default function SignUp(){
         data.append('file', files) 
         data.append('upload_preset', 'c_tags')
         const res = await fetch(
-            "CLOUDINARY_UPLOAD_URL",
+            "https://api.cloudinary.com/v1_1/dotrqr59h/upload",
             {
                 method: "POST",
                 body: data
@@ -58,45 +59,138 @@ export default function SignUp(){
                 password: `${formData.password}`,
             }
         ])
+        if (res.error === null && res.status === 201) {
+            setStatus(true);
+            setFormData(initialState);
+            setTimeout(() => {
+              setStatus(false);
+            }, 5000);
     }
-        return(
-            <div className="flex items-center justify-center w-full h-[100vh] bg-cover bg-center bg-[url('/src/assets/SignUpBackground.jpg')]">
-            <div className="flex flex-col items-center relative w-[400px] rounded-2xl p-9 text-md bg-white shadow-md">
-                <img src="src/assets/textLogo.png" className="h-[40px] mb-2" />
+}
+console.log(loading)
+
+const [visible, setVisible] = useState(true)
+const handleClick = () => {
+    setVisible((prevVisible) => !prevVisible)
+}
+//     const cloudName = import.meta.env.VITE_CLOUD_NAME;          //Variables de sistema
+//     const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+
+//     const [loaded, setLoaded] = useState(false);
+//     const [uploadedImages, setUploadedImages] = useState([]);
+//     const [isDisabled, setIsDisabled] = useState(false);
+
+//     useEffect(() => {
+//         const uwScript = document.getElementById('uw');
+//         if (!loaded && !uwScript) {
+//         const script = document.createElement('script');
+//         script.setAttribute('async', '');
+//         script.setAttribute('id', 'uw');
+//         script.src = 'https://upload-widget.cloudinary.com/global/all.js';
+//         script.addEventListener('load', () => setLoaded(true));
+//         document.body.appendChild(script);
+//         }
+//     }, [loaded]);
+
+//     const processResults = (error:any, result:any) => {
+//         if (result.event === 'close') {
+//         setIsDisabled(false);
+//         }
+//         if (result && result.event === 'success') {
+//         const secureUrl = result.info.secure_url;
+//         const previewUrl = secureUrl.replace(
+//             '/upload/',
+//             '/upload/w_400/f_auto,q_auto/'
+//         );
+//         setUploadedImages((prevImages) => [...prevImages, previewUrl]);
+//         setIsDisabled(false);
+//         }
+//         if (error) {
+//         setIsDisabled(false);
+//         }
+//     };
+
+//     const uploadWidget = () => {
+//         setIsDisabled(true);
+//         window.cloudinary.openUploadWidget(
+//         {
+//             cloudName,
+//             uploadPreset,
+//             sources: ['local', 'url'],
+//             tags: ['myphotoalbum-react'],
+//             clientAllowedFormats: ['image'],
+//             resourceType: 'image',
+//             multiple: false
+//         },
+//         processResults
+//         );
+//     };
+
+
+return(
+        <div className="flex items-center justify-center w-full h-[100vh] lg:bg-cover sm:bg-left  bg-[url('/src/assets/SignUpBackground.png')]">
+            <div className="flex flex-col items-start relative rounded-2xl p-9 text-md bg-white w-[90%] max-w-[900px] bg-opacity-80 backdrop-blur-sm shadow-md">
+                <img src="src/assets/textLogo.png" className="md:h-[40px] md:w-[220px] mb-2 sm:justify-self-center" />
                 <NavLink to="/">
-                    <IoClose size={25} className="text-slate-600 absolute top-2 right-2 hover:scale-105"/>
+                    <IoIosClose  size={28} className="text-slate-400 absolute top-3 right-3 hover:scale-105 hover:text-red-600 transition-all"/>
                 </NavLink>
-                <form className="flex flex-col items-center w-full space-y-3">
-                    <div className="group flex flex-col items-center justify-center h-[100px] w-[100px] rounded-full border hover:bg-slate-100 transition-colors cursor-pointer">
-                        <IoMdImage  size={37} className="text-slate-600 group-hover:scale-105 group-hover:text-violet-600 transition-all"/>
-                        <p className="mt-2 text-xs text-slate-600 text-wrap group-hover:scale-105 group-hover:text-violet-600 transition-all">Añadir foto</p>
-                    </div>  
-                    <p>Foto de perfil</p>
-                    <label className="flex flex-col w-full">
-                        Nombre
-                        <input type="text" className="outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}/>
-                    </label>
-                    <label className="flex flex-col w-full">
-                        Apellido
-                        <input type="text" className="outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}/>
-                    </label>
-                    <label className="flex flex-col w-full">
-                        Dirección de correo
-                        <input type="text" className="outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}/>
-                    </label>
-                    <label className="flex flex-col w-full">
-                        Contraseña
-                        <input type="password" className="outline-none border-t-0 border-2 border-x-0 h-[2.5em] px-2 focus:bg-gray-100 focus:border-violet-600 transition-colors" maxLength={30}/>
-                    </label>
+                <form className="grid w-full space-y-3"
+                      onSubmit={(e:any) => {
+                            e.preventDefault()
+                            createUser()
+                        }}>
+                    <div className="flex flex-col items-center text-sm space-y-2">
+                        <div className="group flex flex-col items-center justify-center self-center h-[100px] w-[100px] rounded-full border border-slate-300 hover:bg-opacity-60 transition-colors cursor-pointer"
+                            onClick={() => handleImage()}>
+                            <IoMdImage  size={37} className="text-slate-600 group-hover:scale-105 group-hover:text-violet-600 transition-all"/>
+                            <p className="mt-2 text-xs text-slate-600 text-wrap group-hover:scale-105 group-hover:text-violet-600 transition-all">Añadir foto</p>
+                        </div>  
+                        <p className="select-none">Foto de perfil</p>
+                        <Separator />
+                    </div>
+                    <div className="w-full grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-6">
+                        <label className="flex flex-col">
+                            <p className="select-none">Nombre</p> 
+                            <input name="firstName" type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent"
+                            onChange={handleChange}/>
+                        </label>
+                        <label className="flex flex-col">
+                            <p className="select-none">Dirección de correo</p> 
+                            <input name="email" type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange}/>
+                        </label>
+                        <label className="flex flex-col">
+                            <p className="select-none">Apellido</p> 
+                            <input name="lastName" type="text" className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange} required/>
+                        </label>
+                        <label className="relative flex flex-col">
+                            <p className="select-none">Contraseña</p>
+                            <input name="password" type={!visible ? "text" : "password"} className="w-[400px] outline-none p-2 rounded-md bg-transparent" maxLength={30}
+                                onChange={handleChange}/>
+                            {visible === true ? (
+                                <IoMdEye size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            ) : (
+                                <IoMdEyeOff size={22} className="absolute top-[33px] right-4 text-slate-500 cursor-pointer" onClick={() => handleClick()}/>
+                            )}
+                        </label>
+                    </div>
+                    <button className="flex justify-self-center justify-center w-[40%] min-w-[100px] mt-[20px] bg-violet-400 px-4 py-2 rounded-md text-white font-semibold hover:bg-violet-500 transition-colors" 
+                            type="submit" 
+                            >
+                        Registrarse
+                    </button>
+                    <p className="justify-self-center w-[200px] mt-8 text-sm text-wrap text-center">¿Ya tienes una cuenta? prueba con <NavLink to="/logIn"><span className="text-violet-600">iniciar sesión</span></NavLink></p>
                 </form>
-                <p className="w-[200px] mt-8 text-sm text-wrap text-center">¿Ya tienes una cuenta? prueba con <NavLink to="/logIn"><span className="text-violet-600">iniciar sesión</span></NavLink></p>
             </div>
-            {status && (
-                <div className="absolute bottom-1 right-1 flex items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-20 translate-y-20">
+            {status === false? (
+                <div className="hidden absolute bottom-2 right-2 items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-20 translate-y-20 transition-transform"></div>
+                ) : (
+                <div className="fixed bottom-2 right-2 flex items-center justify-around w-[300px] h-[50px] bg-white p-2 text-md rounded-md translate-x-0 translate-y-0 transition-transform">
                     <FaCircleCheck size={20} className="text-green-400" />
                     Se ha registrado correctamente
                 </div>
-            )}
+                )}
         </div>
     )
 }
