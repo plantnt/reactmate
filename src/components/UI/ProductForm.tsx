bac6990606eaeb901dd074b3227688b06d7ec1a5
plantnt <godzillagmk2204@gmail.com>
2024-03-20 14:33:39 -0500

    deleted:    src/components/UI/ProductForm.css 	modified:   src/components/UI/ProductForm.tsx 	modified:   src/pages/ProductUpload.tsx

--- src/components/UI/ProductForm.tsx
+++ src/components/UI/ProductForm.tsx
@@ -1,65 +1,118 @@
 import type { SelectProps } from 'antd';
 import { Select, Space } from 'antd';
+import { useState } from 'react';
+import { MdAttachMoney } from 'react-icons/md';
 
-import "./ProductForm.css"
 
 const ProductForm = () => {
     
     const handleChange = (value: string[]) => {
         console.log(`selected ${value}`);
     };
-    const options: SelectProps['options'] = [
+    const material: SelectProps['options'] = [
         {
-          label: 'Mueble de madera',
+          label: 'Madera',
           value: '1',
-          desc: 'Mueble de madera',
+          desc: 'Madera',
         },
         {
-          label: 'Mueble de plástico',
+          label: 'Plástico',
           value: '2',
-          desc: 'Mueble de plástico',
+          desc: 'Plástico',
+        },
+        {
+          label: 'Madera reciclada',
+          value: '3',
+          desc: 'Madera reciclada',
+        },
+        {
+          label: 'Plástico reciclado',
+          value: '4',
+          desc: 'Plástico reciclado',
+        },
+        {
+          label: 'Tela',
+          value: '5',
+          desc: 'Tela',
+        },
+      ];
+
+    const categoria: SelectProps['options'] = [
+        {
+          label: 'Mueble de sala',
+          value: '1',
+          desc: 'Mueble de sala',
+        },
+        {
+          label: 'Mueble de exteriores',
+          value: '2',
+          desc: 'Mueble de exteriores',
+        },
+        {
+          label: 'Mueble de cocina',
+          value: '3',
+          desc: 'Mueble de cocina',
         },
         {
           label: 'Mueble restaurado',
-          value: '3',
+          value: '4',
           desc: 'Mueble restaurado',
         },
         {
-          label: 'Mueble de jardín',
-          value: '4',
-          desc: 'Mueble de jardín',
+          label: 'Mueble de playa',
+          value: '5',
+          desc: 'Mueble de playa',
         },
       ];
+
+      const [value, setValue] = useState('')
+      const handleInput = (event: { target: { value: string } }) => {
+
+      const result = event.target.value.replace(/\D/g, '')
+        setValue(result)
+      }
           
     document.querySelector("#select")?.classList.remove(".css-b62m3t-container")
     return(
-        <div className="h-[400px] flex flex-col">
-            <label>
+        <div className="h-[400px] w-[400px] space-y-5 flex flex-col">
+            <label className='grid'>
                 Titulo
-                <input type="text" className="border-2 " />
+                <input type="text" className="px-1 border-2 rounded-md outline-none w-full h-[2.4em]" maxLength={50}/>
             </label>
-            <label>
+            <label className='grid'>
                 Precio
-                <input type="text" className="border-2 " />
+                <span className='flex items-center'>
+                  COP
+                  <MdAttachMoney size={25}/>
+                  <input type="number" min="0.01" step="0.01" max="1000000000" onChange={handleInput} value={value} className="px-1 border-2 rounded-md outline-none w-full h-[2.4em]" />
+                </span>
             </label>
-            <label>
-                Categorías
+            <label className='grid'>
+                Material
                 <Select
-                mode="multiple"
                 style={{ width: '200px' }}
                 placeholder="Seleccione una categoría"
                 onChange={handleChange}
                 optionLabelProp="label"
-                options={options}
+                options={material}
                 optionRender={(option) => (
                 <Space>
-                    <span role="img" aria-label={option.data.label}>
-                    {option.data.emoji}
-                    </span>
                     {option.data.desc}
                 </Space>
-                )}
-            />
+                )}/>
+                  Categoría
+                  <Select
+                  mode="multiple"
+                  style={{ width: '200px' }}
+                  placeholder="Seleccione una categoría"
+                  onChange={handleChange}
+                  optionLabelProp="label"
+                  options={categoria}
+                  optionRender={(option) => (
+                  <Space>
+                      {option.data.desc}
+                  </Space>
+                  )}/>
             </label>
         </div>
     )
