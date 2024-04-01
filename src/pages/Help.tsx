0425bec8549ed2e364fc69505354a3fabd06c2fe
plantnt <godzillagmk2204@gmail.com>
2024-04-01 15:17:16 -0500

    modified:   src/pages/Help.tsx

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -6,6 +6,10 @@ export default function HelpPage(){
             Dia numero 450<br/>
              Hoy se acabo el agua. No se cuanto más voy a soportar estar encerrado detras de cuatro metros de hormigon reforzado, se supone que fue hecha para protegernos, pero ahora la culpo de mi locura. Nos quedaban dos botellas de agua, pero una fue destinada a los cultivos, o los que quedan. Ayer a las tres de la tarde bebimos la ultima gota. El golpe de realidad más duro que hemos tenido desde que toda esta mierda comenzó.
         </div>
+        <div className="text-justify font-bold bg-gradient-to-l from-yellow-100 to-yellow-800">
+            Dia numero 462<br/>
+             Hace unos días, si es que mi memoria no ha dejado de funcionar, encontré un poso de agua, mis instintos más salvajes me impulsaron a beberla, sin siquiera antes asegurarme que era limpia. Probablemente no lo estaba, pero el efecto placebo de haber sasiado mi sed, me permite estar cuerdo por un día más.
+        </div>
         </>
         
         
