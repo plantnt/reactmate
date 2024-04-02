d4af2fe54148311737bde094089f26873dd57f5a
plantnt <godzillagmk2204@gmail.com>
2024-04-02 08:16:08 -0500

    modified:   src/components/UI/navbar.tsx

--- src/components/UI/navbar.tsx
+++ src/components/UI/navbar.tsx
@@ -3,64 +3,85 @@ import { useState } from 'react';
 
 import { FaCommentDots, } from 'react-icons/fa';
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 import cartIcon from '../../assets/cartIcon.png';
 import profileIcon from '../../assets/profileIcon.png';
 import textLogo from '../../assets/textLogo.png';
+import furniLogo from '../../assets/furniLogo.png';
 
 import "./ui.css";
 
 const Navbar = () => {
     const [logged, setLogged] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
+
+    const handleBurgerMenu = () => {
+        setShowMenu((prevMenu) => !prevMenu)
+    }
     return (
-        <nav className="sticky top-0 w-full bg-white border-slate-300 border-b-[1px] z-50">
-            <div className="container w-full">
-                <div className="flex flex-row py-2 items-center">
-                    <div className="basis-1/5">
-                        <Link to='/'><img src={textLogo} className="min-w-[120px] h-[30px] m-2 justify-self-start pl-6"/></Link>
-                    </div>
-                    <div className="basis-2/5 flex items-center">
-                        <input type="text" className="bg-slate-200 rounded-l-3xl outline-none px-2 h-7 text-gray-900 w-[85%]"/>
-                        <button className="rounded-r-3xl h-7 w-14 bg-furnipurple cursor-pointer">
-                        <FaMagnifyingGlass className="text-white ml-[18.5px]"/>
-                        </button>
-                        
-                    </div>
-                    <div className="basis-2/5">     
-                        <ul className="flex flex-row p-2 justify-evenly items-center">
-                            <li>
-                                <NavLink to='/cartPage'><img src={cartIcon} className="min-w-[35px] max-h-[30px]" /></NavLink>
-                            </li>
-                            <li>
-                                {/* Insert the chat button here */}
-                                    <NavLink to='/chatHome'>
-                                    <FaCommentDots className="text-purple-400 text-4xl" /> {/* Cambiar el color y el tamaño */}
-                                    </NavLink>
-                            </li>
-                            {logged === true && 
-                            <li>
-                                <NavLink to='/profilePage'><img src={profileIcon} width="35" className=""/></NavLink>
-                            </li>
-                            }
-                            <li>
-                                <div className="flex flex-row ml-5 space-x-4">
-                                    <NavLink to='/logIn'>
-                                        <button className="flex justify-center items-center bg-[#945aff] hover:bg-[#7b5aff] px-3 h-10 w-[120px] text-white font-semibold hover:font-black rounded-2xl transition-all">
-                                            ENTRAR
-                                        </button>
-                                    </NavLink>
-                                    <NavLink to='/signUp'>
-                                        <button className="flex justify-center items-center bg-[#ff6c6c] hover:bg-[#f65858] px-3 h-10 w-[120px] text-white font-semibold hover:font-black rounded-2xl transition-all">
-                                            REGISTRARSE
-                                        </button>
-                                    </NavLink>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+        <div className='relative'>
+            <nav className="sticky top-0 flex sm:justify-center justify-between items-center space-x-9 w-full p-3 bg-white border-slate-300 border-b-[1px] z-50">
+                <NavLink to='/'>
+                    <img src={textLogo} className=" sm:block w-36 h-auto hidden"/>
+                    <img src={furniLogo} className=" block w-16 h-auto sm:hidden"/>
+                </NavLink>
+                <div className='sm:w-[30%] w-[10%] flex items-center4'>
+                    <input type="text" className="bg-slate-200 rounded-l-3xl outline-none px-2 h-7 text-gray-900 w-[85%] hidden sm:block"/>
+                    <button className="flex justify-center items-center sm:rounded-l-none rounded-3xl  h-7 w-14 bg-furnipurple cursor-pointer">
+                        <FaMagnifyingGlass className="text-white"/>
+                    </button>
                 </div>
+                <RxHamburgerMenu size={30} className=" text-convenientOrange cursor-pointer sm:hidden block"
+                onClick={handleBurgerMenu}/>
+                <div className="hidden sm:flex">     
+                    <ul className="flex p-2 space-x-3 items-center">
+                        <li>
+                            <NavLink to='/cartPage'>
+                                <img src={cartIcon} className="w-[35px] h-auto" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/chatHome'>
+                                <FaCommentDots size={35} className="text-convenientPurple w-[70px]" /> 
+                            </NavLink>
+                        </li>
+                        {logged === true ? 
+                        <li>
+                            <NavLink to='/profilePage'>
+                                <img src={profileIcon} className="w-[35px] h-auto"/>
+                            </NavLink>
+                        </li>
+                        :
+                        <li>
+                            <div className="flex flex-row space-x-3">
+                                <NavLink to='/logIn'>
+                                    <button className="flex justify-center items-center bg-[#945aff] hover:bg-[#7b5aff] px-3 h-10 w-[120px] text-white font-semibold hover:font-black rounded-2xl transition-all">
+                                        ENTRAR
+                                    </button>
+                                </NavLink>
+                                <NavLink to='/signUp'>
+                                    <button className="flex justify-center items-center bg-[#ff6c6c] hover:bg-[#f65858] px-3 h-10 w-[120px] text-white font-semibold hover:font-black rounded-2xl transition-all">
+                                        REGISTRARSE
+                                    </button>
+                                </NavLink>
+                            </div>
+                        </li>
+                        }
+                    </ul>
+                </div>
+            </nav>
+            {showMenu &&
+            <div className='sticky w-full bg-white z-50'>
+                <ul>
+                    <li>Op</li>
+                    <li>Op</li>
+                    <li>Op</li>
+                    <li>Op</li>
+                </ul>
             </div>
-        </nav>
+            }
+        </div>
     );
 }
 
