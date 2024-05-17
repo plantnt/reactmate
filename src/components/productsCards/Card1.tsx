741741cf304d8f865c8c8b9317ab5356a5b5433e
plantnt <godzillagmk2204@gmail.com>
2024-05-17 15:13:29 -0500

    modified:   src/components/productsCards/Card1.tsx 	modified:   src/components/productsCards/Card2.tsx 	modified:   src/components/productsCards/Card3.tsx 	modified:   src/components/productsCards/Card4.tsx 	modified:   src/components/productsCards/Card5.tsx

--- src/components/productsCards/Card1.tsx
+++ src/components/productsCards/Card1.tsx
@@ -1,19 +1,20 @@
 import { NavLink } from "react-router-dom"
-import Card from 'react-bootstrap/Card';
+import { Rate } from "antd";
 
 export default function Card1(){
     return(
         <>
-            <NavLink to="/productPage">
-                <Card className="w-[10rem] bg-white color-black border-[1px] rounded-lg shadow-md">
-                    <Card.Img src="src\assets\products\1.jpg" />
-                    <Card.Body className="p-3">
-                        <Card.Title>Titulo</Card.Title>
-                        <Card.Text className="font-bold">
-                            COP $999.99
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+            <NavLink to={'/productPage'}>
+                <div className='w-[10rem] bg-white border-2 border-slate-200 rounded-lg shadow-md'>
+                    <div className="h-[140px] w-full overflow-hidden">
+                        <img src="src\assets\products\1.jpg" alt="Imagen product" className='object-cover'/>
+                    </div>
+                    <div className='w-full p-2 bg-white'>
+                        <h3 className='font-semibold text-wrap'>Mueble azul de casa</h3>
+                        <Rate allowHalf disabled value={5} className="text-violet-400 text-xs" />
+                        <h1 className=''>$ COP 999.99</h1>
+                    </div>
+                </div>
             </NavLink>
         </>
     )
