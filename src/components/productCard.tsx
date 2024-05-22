c4685b0c76376951cf73a9b21a3832f5c3e72624
alejiaet <alejiaet@gmail.com>
2024-05-22 14:59:46 -0500

    modified:   src/components/productCard.tsx modified:   src/components/rating.tsx

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -39,26 +39,29 @@ const ProductCard = () => {
 
     const logo = Math.round(Math.random()*(5 - 0));
 
+    
+
     return(
         <>
             <NavLink to={'/productPage'}>
-                <div className='w-[10rem] bg-white border-2 border-slate-200 rounded-lg shadow-md'>
+                
+                      <div className='w-[10rem] bg-white border-2 border-slate-200 rounded-lg shadow-md'>
 
-                <div className="h-[140px] w-full overflow-hidden relative">
-                    {logo > 3 ? (
-                        <FaLeaf size={25} className="text-[#27ff59] w-[30px] h[30px] block absolute top-0 left-0  bg-white rounded-md p-1"/>
-                    ) : (
-                        ''
-                    ) }
-                        <img src={randomImage} alt="Imagen product" className='object-cover'/>
-                    </div>
-    
-                    <div className='w-full p-2 bg-white'>
-                    <Rate allowHalf disabled value={values} className="text-violet-400 pt-1 text-xs text-center bg-purple-200 rounded-b-full px-1 w-full" />
-                        <h3 className='font-semibold text-wrap truncate'>Mueble azul de casa</h3>
-                        <h1 className='text-lg'>COP ${price1},{price2}</h1>
-                    </div>
-                </div>
+                      <div className="h-[140px] w-full overflow-hidden relative">
+                          {logo > 3 ? (
+                              <FaLeaf size={25} className="text-[#27ff59] w-[30px] h[30px] block absolute top-0 left-0  bg-white rounded-md p-1"/>
+                          ) : (
+                              ''
+                          ) }
+                              <img src={randomImage} alt="Imagen product" className='object-cover'/>
+                          </div>
+          
+                          <div className='w-full p-2 bg-white'>
+                          <Rate allowHalf disabled value={values} className="text-violet-400 pt-1 text-xs text-center bg-purple-200 rounded-b-full px-1 w-full" />
+                              <h3 className='font-semibold text-wrap truncate'>Mueble azul de casa</h3>
+                              <h1 className='text-lg'>COP ${price1},{price2}</h1>
+                          </div>
+                      </div>                
             </NavLink>
         </>
     )
